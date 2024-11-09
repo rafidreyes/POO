a0c109ea1be43de7132770802fd9f8464a8f0b17
R4f4Reyes <rdreyes1@uth.hn>
2024-11-09 14:51:27 -0600

    Pueba Clase

---
+++ src/main/java/POO/Herencia.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package POO;
+
+/**
+ *
+ * @author Rafael Reyes
+ */
+public class Herencia {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
